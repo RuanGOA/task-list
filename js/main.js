8a48a737725e609c607a5fc98332a9609b2a271f
ruangoa <ruan.alves@ccc.ufcg.edu.br>
2020-04-22 15:27:26 -0300

    add script

---
+++ js/main.js
@@ -0,0 +1,66 @@
+var inputElement = document.querySelector('#main main input');
+var buttonElement = document.querySelector('#main main button');
+var listElement = document.querySelector('#main main ul');
+
+var roster = [];
+
+function render() {
+   listElement.innerHTML = '';
+
+   for(todo of roster) {
+      var pos = roster.indexOf(todo);
+
+      listElement.appendChild(createLi(todo, pos));
+   }
+}
+
+render();
+
+function createLi(text, pos) {
+   //create <a> element
+   var aElement = document.createElement('a');
+   aElement.setAttribute('href', '#');
+   aElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
+   //create <a> text
+   String.fromCodePoint(0x1F621)
+   var aText = document
+      .createTextNode('  ' +
+         String.fromCodePoint(0x274C)
+      );
+   //fin. <a>
+   aElement.appendChild(aText);
+
+   //create <p> element
+   var pElement = document.createElement('p');
+   //create text of <p>
+   var pText = document.createTextNode(text);
+   //fin. <p>
+   pElement.appendChild(pText);
+
+   //add <a> to <p>
+   pElement.appendChild(aElement);
+
+   //create <li> element
+   var liElement = document.createElement('li');
+   
+   //add <p> to <li>
+   liElement.appendChild(pElement);
+
+   return liElement;
+}
+
+function addTodo() {
+   var todoText = inputElement.value;
+
+   roster.push(todoText);
+   inputElement.value = '';
+
+   render();
+}
+
+buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+   roster.splice(pos, 1);
+   render();
+}
